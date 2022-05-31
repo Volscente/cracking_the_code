3a3287233aff618d0ace060bedf15868369e1716
Simone Porreca <porrecasimone@gmail.com>
2022-05-31 09:22:18 +0200

    Tree - Average Depth First Search

---
+++ trees/average_depth_first_search.py
@@ -0,0 +1,78 @@
+"""
+Problem description:
+Return the average of the node's values at each depth level in a Binary Tree.
+
+        4
+       | \
+      6   10
+     | \   \
+    3   4   11
+              \
+               4
+
+output: [4, 8, 6, 4]
+"""
+
+
+class Node:
+
+    def __init__(self, value):
+
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+
+def _breadth_first_search(node, dict, depth=0):
+
+    if node is None:
+
+        return None
+
+    if depth not in dict.keys():
+
+        dict[depth] = []
+
+    dict[depth].append(node.value)
+
+    _breadth_first_search(node.left_child, dict, depth + 1)
+    _breadth_first_search(node.right_child, dict, depth + 1)
+
+
+def avg_by_depth(node):
+
+    avg_dict = {}
+    output = []
+
+    _breadth_first_search(node, avg_dict)
+
+    for depth in avg_dict.keys():
+
+        depth_sum = sum(avg_dict[depth])
+        avg = depth_sum / len(avg_dict[depth])
+        output.append(int(avg))
+
+    print(avg_dict)
+    print(output)
+
+
+if __name__ == '__main__':
+
+    node_root = Node(4)
+    node_1 = Node(6)
+    node_2 = Node(10)
+    node_3 = Node(3)
+    node_4 = Node(4)
+    node_5 = Node(11)
+    node_6 = Node(4)
+
+    node_root.left_child = node_1
+    node_root.right_child = node_2
+    node_1.left_child = node_3
+    node_1.right_child = node_4
+    node_2.right_child = node_5
+    node_5.right_child = node_6
+
+    avg_by_depth(node_root)
+
+
