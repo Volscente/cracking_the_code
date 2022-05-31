e4214632441d0407d9d83c526c47462cc48b412a
Simone Porreca <porrecasimone@gmail.com>
2022-05-31 16:59:06 +0200

    Update longest_substring_distinct_chars.py

--- sliding_windows/longest_substring_distinct_chars.py
+++ sliding_windows/longest_substring_distinct_chars.py
@@ -16,3 +16,29 @@ Input: String="abccde"
 Output: 3
 Explanation: Longest substrings with distinct characters are "abc" & "cde".
 """
+
+
+def non_repeat_substring(string):
+    max_length = 0
+    char_dict = {}
+
+    window_start = 0
+
+    for window_end in range(len(string)):
+
+        if string[window_end] not in char_dict.keys():
+
+            char_dict[string[window_end]] = 1
+
+        else:
+
+            char_dict[string[window_end]] += 1
+
+        while char_dict[string[window_end]] > 1:
+
+            char_dict[string[window_start]] -= 1
+
+            if char_dict[string[window_start]] == 0:
+                del char_dict[string[window_start]]
+
+            window_start += 1
