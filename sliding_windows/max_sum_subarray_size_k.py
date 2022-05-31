9376fb2f20b6706df2d7e31f28534ee8daaa599a
Simone Porreca <porrecasimone@gmail.com>
2022-05-31 13:20:31 +0200

    Update max_sum_subarray_size_k.py

--- sliding_windows/max_sum_subarray_size_k.py
+++ sliding_windows/max_sum_subarray_size_k.py
@@ -10,11 +10,13 @@ Example 2
 Input: [2, 3, 4, 1, 5], k=2
 Output: 7
 Explanation: Subarray with maximum sum is [3, 4].
+
+Time Complexity: O(N) -> All the N elements of the array have to be looped
+Space Complexity: O(1)
 """
 
 
 def max_sub_array_of_size_k(k, arr):
-
     max_value = 0
     max_sequence = []
 
@@ -35,3 +37,12 @@ def max_sub_array_of_size_k(k, arr):
             window_start += 1
 
     return max_value
+
+
+def main():
+
+    print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+    print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
+
+
+main()
