d874dba9b963a693678ccc5d2e2057c470f97d4b
Simone Porreca <porrecasimone@gmail.com>
2022-05-31 15:37:32 +0200

    Create longest_substring_max_k_distinct_chars.py

---
+++ sliding_windows/longest_substring_max_k_distinct_chars.py
@@ -0,0 +1,35 @@
+def longest_substring_with_k_distinct(str1, k):
+    """
+    Time Complexity: O(N)
+    Space Complexity: O(K) (Where K is the number of characters within the substring)
+    :param str1: String
+    :param k: Max number of distinct characters
+    :return:
+    """
+    longest_substring = 0
+    window_start = 0
+    substring = []
+
+    for window_end in range(len(str1)):
+
+        substring += str1[window_end]
+
+        if len(set(substring)) <= k:
+
+            longest_substring = max(longest_substring, len(substring))
+
+        else:
+
+            substring.pop(0)
+            window_start += 1
+
+    return longest_substring
+
+
+def main():
+    print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
+
+
+main()
