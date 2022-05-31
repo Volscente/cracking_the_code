b6025aa020d4fe738f4cfef9f5f3c054600df508
Simone Porreca <porrecasimone@gmail.com>
2022-05-31 14:37:39 +0200

    Update smallest_subarray_greater_sum.py

--- sliding_windows/smallest_subarray_greater_sum.py
+++ sliding_windows/smallest_subarray_greater_sum.py
@@ -36,24 +36,28 @@ def smallest_subarray_sum_improved(s, arr):
     :param arr:
     :return:
     """
+
     min_length = math.inf
-    window_sum = 0
     window_start = 0
+    window_sum = 0
 
-    for window_end in range(0, len(arr)):
+    for window_end in range(len(arr)):
 
-        window_sum += arr[window_end]  # add the next element
-        # shrink the window as small as possible until the 'window_sum' is smaller than 's'
+        window_sum += arr[window_end]
 
         while window_sum >= s:
-            min_length = min(min_length, window_end - window_start + 1)
+
+            min_length = min(min_length, window_end-window_start + 1)
             window_sum -= arr[window_start]
             window_start += 1
 
     if min_length == math.inf:
+
         return 0
 
-    return min_length
+    else:
+
+        return min_length
 
 
 def main():
