c3ad1a5e26b858212273ce1bbbd6088c3ad2ae25
Simone Porreca <porrecasimone@gmail.com>
2022-06-04 10:10:04 +0200

    BFS - Time and Space Complexity

--- breadth_first_search/binary_tree_level_order_traversal.py
+++ breadth_first_search/binary_tree_level_order_traversal.py
@@ -1,3 +1,12 @@
+"""
+Given a binary tree, populate an array to represent its level-by-level traversal.
+You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+"""
+
+
 from collections import deque
 
 
