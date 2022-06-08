878ae1d8342d6606a7cb9df51ea15a8838446a01
Simone Porreca <porrecasimone@gmail.com>
2022-06-08 13:44:10 +0200

    Create 4_next_letter.py

---
+++ modified_binary_search/4_next_letter.py
@@ -0,0 +1,66 @@
+"""
+
+Problem Statement#
+Given an array of lowercase letters sorted in ascending order,
+find the smallest letter in the given array greater than a given ‘key’.
+
+Assume the given array is a circular list, which means that the last letter is assumed to be connected with the first letter.
+This also means that the smallest letter in the given array is greater than the last letter of the array and is also the first letter of the array.
+
+Write a function to return the next letter of the given ‘key’.
+
+Example 1:
+Input: ['a', 'c', 'f', 'h'], key = 'f'
+Output: 'h'
+Explanation: The smallest letter greater than 'f' is 'h' in the given array.
+
+Example 2:
+Input: ['a', 'c', 'f', 'h'], key = 'b'
+Output: 'c'
+Explanation: The smallest letter greater than 'b' is 'c'.
+
+Example 3:
+Input: ['a', 'c', 'f', 'h'], key = 'm'
+Output: 'a'
+Explanation: As the array is assumed to be circular, the smallest letter greater than 'm' is 'a'.
+
+Example 4:
+Input: ['a', 'c', 'f', 'h'], key = 'h'
+Output: 'a'
+Explanation: As the array is assumed to be circular, the smallest letter greater than 'h' is 'a'.
+"""
+
+
+def search_next_letter(letters, key):
+
+    start, end = 0, len(letters) - 1
+
+    if key >= letters[end]:
+        return letters[start]
+
+    while start <= end:
+
+        middle = start + (end - start) // 2
+
+        if key < letters[middle]:
+
+            end = middle - 1
+
+        elif key > letters[middle]:
+
+            start = middle + 1
+
+        else:
+
+            return letters[middle + 1]
+
+    return letters[end + 1]
+
+
+def main():
+    print(search_next_letter(['a', 'c', 'f', 'h'], 'f'))
+    print(search_next_letter(['a', 'c', 'f', 'h'], 'b'))
+    print(search_next_letter(['a', 'c', 'f', 'h'], 'm'))
+
+
+main()
