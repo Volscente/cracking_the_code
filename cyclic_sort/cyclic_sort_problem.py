c55c041866da4e9d18da9b61c04a7ea27c834d02
Simone Porreca <porrecasimone@gmail.com>
2022-07-06 17:49:36 +0200

    Update cyclic_sort_problem.py

--- cyclic_sort/cyclic_sort_problem.py
+++ cyclic_sort/cyclic_sort_problem.py
@@ -3,7 +3,8 @@ We are given an array containing n objects. Each object, when created, was assig
 1 to n based on their creation sequence. This means that the object with sequence number 3 was
 created just before the object with sequence number 4.
 
-Write a function to sort the objects in-place on their creation sequence number in O(n) and without using any extra space.
+Write a function to sort the objects in-place on their creation sequence number in O(n)
+and without using any extra space.
 For simplicity, let’s assume we are passed an integer array containing only the sequence numbers,
 though each number is actually an object.
 
