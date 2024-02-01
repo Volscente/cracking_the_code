f3c167eb43624cf208c105e4c7d245b03cac2bfc
Simone Porreca <porrecasimone@gmail.com>
2024-02-01 21:48:25 +0100

    Update exercises.py

--- two_pointers/exercises.py
+++ two_pointers/exercises.py
@@ -3,27 +3,52 @@ def find_target_sum(arr, target_sum):
     Given an ordered array of numbers, find the indices of the two number within it that corresponds to a target sum
     """
 
-    left, right = 0, len(arr) - 1
+    start, end = 0, len(arr) - 1
 
-    while (left < right):
+    while start < end:
 
-        computed_sum = arr[left] + arr[right]
+        computed_sum = arr[start] + arr[end]
 
         if computed_sum == target_sum:
 
-            return [left, right]
+            return [start, end]
         
-        elif target_sum > computed_sum:
+        elif computed_sum > target_sum:
 
-            left += 1
+            end -= 1
 
         else:
 
-            right -= 1
+            start += 1
 
     return [-1, -1]
 
+def remove_duplicates(arr):
+
+
+    start, end = 0, 1
+
+    for i in range(len(arr)):
+
+        if arr[start] > arr[end]:
+
+            return len(arr[:start + 1])
+
+        if arr[start] == arr[end]:
+
+            duplicated = arr.pop(end)
+            arr.append(duplicated)
+        
+        else:
+
+            start += 1
+            end += 1
+            
+    return len(arr[:start + 1])
+
 
 print(find_target_sum([1, 2, 3, 4, 6], 6)) # [1, 3]
+
+print(remove_duplicates([2, 3, 3, 3, 6, 9, 9])) # 4
 
         
