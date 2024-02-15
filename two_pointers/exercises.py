c207356f4eb134023002a3733ca08405e0aa573f
Simone Porreca <porrecasimone@gmail.com>
2024-02-15 16:19:55 +0100

    Update exercises.py

--- two_pointers/exercises.py
+++ two_pointers/exercises.py
@@ -50,11 +50,11 @@ def remove_duplicates(arr):
 
 def make_squares(arr):
 
-    square_arr = [0 for x in range(len(arr))]
-
-    left, right = 0, len(arr) - 1
+    squares = [0 for x in range(len(arr))]
 
     highest_square_index = len(arr) - 1
+
+    left, right = 0, len(arr) - 1
 
     while left <= right:
 
@@ -63,18 +63,57 @@ def make_squares(arr):
 
         if square_left > square_right:
 
-            square_arr[highest_square_index] = square_left
+            squares[highest_square_index] = square_left
             left += 1
 
         else:
 
-            square_arr[highest_square_index] = square_right
+            squares[highest_square_index] = square_right
             right -= 1
 
         highest_square_index -= 1
+    
+    return squares
 
-    return square_arr
+def find_triplet_sum_to_zero(arr):
 
+    arr.sort()
+
+    triplets = []
+
+    for i in range(len(arr)):
+
+        if i > 0  and arr[i] == arr[i - 1]:
+            continue
+
+        find_non_duplicated_pair_target_sum(arr, -arr[i], i + 1, triplets)
+    
+    return triplets
+
+def find_non_duplicated_pair_target_sum(arr, target_sum, left, triplets):
+
+    right = len(arr) - 1
+
+    while left < right:
+
+        computed_sum = arr[left] + arr[right]
+
+        if computed_sum == target_sum:
+
+            triplets.append([-target_sum, arr[left], arr[right]])
+
+            left += 1
+            right -= 1
+
+            while left < right and arr[left] == arr[left - 1]:
+                left +=1
+            while left < right and arr[right] == arr[right + 1]:
+                right -= 1
+        
+        elif computed_sum > target_sum:
+            right -= 1
+        elif computed_sum < target_sum:
+            left += 1
 
 
 
@@ -83,5 +122,7 @@ print(find_target_sum([1, 2, 3, 4, 6], 6)) # [1, 3]
 print(remove_duplicates([2, 3, 3, 3, 6, 9, 9])) # 4
 
 print(make_squares([-2, -1, 0, 2, 3])) # [0, 1, 4, 4, 9]
+
+print(find_triplet_sum_to_zero([-3, 0, 1, 2, -1, 1, -2])) # [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
 
         
