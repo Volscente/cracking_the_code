5395bf153204c54045e70ad683583bc2bf2c1de6
Simone Porreca <porrecasimone@gmail.com>
2024-07-22 19:43:07 +0200

    Add exercise review

--- breadth_first_search/binary_tree_level_order_traversal.py
+++ breadth_first_search/binary_tree_level_order_traversal.py
@@ -3,7 +3,7 @@ Given a binary tree, populate an array to represent its level-by-level traversal
 You should populate the values of all nodes of each level from left to right in separate sub-arrays.
 
 Time Complexity: O(N)
-Space Complexity: O(N)
+Space Complexity: O(W) (W: max number of nodes across all levels)
 """
 
 
